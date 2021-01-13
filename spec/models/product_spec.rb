0c52be21a45d63f39c5e7f285789b8257df68a9f
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-01-13 09:59:37 +0000

    Complete rspec for model Product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,49 +16,46 @@ RSpec.describe Product, type: :model do
 
     it 'validates :name, presence: true' do
       new_category = Category.create! name: 'Movies'
-      expect(new_category.products.create!({
+      new_product = new_category.products.create(
+        name: nil,
         quantity: 5,
         price: 50.00
-      })).to_not be_valid
-
-      expect(new_product[:name]).to_not be_valid
-      # expect(nil.errors.message[:nil]).to eq ['is invalid']
+      )
+      
+      expect(new_product.errors.full_messages).to_not be_empty
     end
 
     it 'validates :price, presence: true' do
       new_category = Category.create! name: 'Music'
-      new_product = new_category.products.create!({
+      new_product = new_category.products.create(
         name: 'Red to Purple',
         quantity: 21,
-        price: 15.00
-      })
+        price: nil
+      )
       
-      expect(nil).to_not be_valid
-      # expect(nil.errors.message[:nil]).to eq ['is invalid']
+      expect(new_product.errors.full_messages).to_not be_empty
     end
 
     it 'validates :quantity, presence: true' do
       new_category = Category.create! name: 'Accessories'
-      new_product = new_category.products.create!({
+      new_product = new_category.products.create(
         name: 'Most rugged phone case',
-        quantity: 17,
+        quantity: nil,
         price: 45.99
-      })
+      )
       
-      expect(nil).to_not be_valid
-      # expect(nil.errors.message[:nil]).to eq ['is invalid']
+      expect(new_product.errors.full_messages).to_not be_empty
     end
 
     it 'validates :category, presence: true' do
       new_category = Category.create! name: 'Office Supplies'
-      new_product = new_category.products.create!({
+      new_product = Product.create(
         name: 'Never drying printer ink',
         quantity: 20,
         price: 69.99
-      })
-      
-      expect(nil).to_not be_valid
-      # expect(nil.errors.message[:nil]).to eq ['is invalid']
+      )
+
+      expect(new_product.errors.full_messages).to_not be_empty
     end
   end
 end
